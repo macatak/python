4d35534d122561aaec39aa4f1a9d8212ccc87e1b
macatak <mdmclauchlin@gmail.com>
2023-09-17 17:31:49 -0600

    Update fileIO.py

--- python-101/fileIO.py
+++ python-101/fileIO.py
@@ -12,6 +12,14 @@ for line in inReadFile:
     print(line, end='')
     outWriteFile.write(line)
 
+# use a variable in a filename spamANDeggs.txt
+# uses print format
+tmpVariable1 = 'spam'
+tmpVariable2 = 'eggs'
+outWriteVariable = open('{}AND{}.txt'.format(tmpVariable1, tmpVariable2), 'w')
+outWriteVariable.write('spammy')
+
 # closing the open file
 inReadFile.close()
 outWriteFile.close()
+outWriteVariable.close()
